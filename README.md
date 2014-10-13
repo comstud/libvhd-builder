b4f6c884447c7225c8f2c54d2f4e84f9beffeb4b
Chris Behrens <cbehrens@codestud.com>
2014-10-13 13:22:41 -0700

    Add the script
    ...and update the README.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 libvhd-builder
 ==============
 
-libvhd build script
+libvhd build script. Simple script to checkout blktap code and compile
+the vhd library and utils. This builds my version which adds a vhdutil
+command to convert to raw.
---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Requires:
+# uuid-dev
+# libaio-dev
+
+blktap_repo='git@github.com:comstud/blktap'
+branch='add_vhd2raw'
+
+if [ ! -d blktap.git ] ; then
+    git clone ${blktap_repo} blktap.git
+    if [ $? -ne 0 ] ; then
+        echo "Error: Couldn't clone ${blktap_repo}."
+        exit 1
+    fi
+fi
+
+cd blktap.git || exit 1
+git checkout ${branch} || exit 1
+
+if [ ! -f vhd/Makefile ] ; then
+    ./autogen.sh &&
+    ./configure
+    if [ $? -ne 0 ] ; then
+        echo 'Error: configure failed.'
+        exit 1
+    fi
+fi
+
+cd vhd && make
+if [ $? -ne 0 ] ; then
+    echo 'Error: Build failed.'
+    exit 1
+fi
